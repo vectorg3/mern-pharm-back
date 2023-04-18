20db845cd727fd007a1fc58fad286a0bb9c309ee
vectorg3 <diglytail@gmail.com>
2023-04-18 19:13:10 +0300

    fixed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.set('strictQuery', false);
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => {
-        console.log('DB OK');
+        console.log('DB OKe');
     })
     .catch((err) => {
         console.log('DB ERROR', err);
